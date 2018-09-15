75803f5398d545c09134feb221a85c1ef68915a2
jessica-dl <jessica.deleeuw2@gmail.com>
2018-09-15 16:29:03 -0400

    Added some styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,22 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(params.require(:article).permit(:title, :text))
     # render plain: params[:article].inspect
     @article.save
-    retiderct_to @article
+    redirect_to @article
   end
+
+  private
+     def article_params
+       params.require(:article).permit(:title, :text)
+     end
 
 end
