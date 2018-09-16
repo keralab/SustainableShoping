395f2bec61513620d898263aefd23119dca96011
Kerala Hough Brendon <keralabrendon@gmail.com>
2018-09-16 00:09:16 -0400

    Working search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,22 +1,32 @@
 require 'uri'
 require 'net/http'
 require 'net/https'
-
+require 'rubygems'
+require 'json'
 class WelcomeController < ApplicationController
   def search
-    url = URI("https://gateway-staging.ncrcloud.com/catalog/items?merchandiseCategoryId=fair-trade")
+    @results = []
+    if params[:keyword]
+      url = URI("https://gateway-staging.ncrcloud.com/catalog/items/suggestions?descriptionPattern=*#{params[:keyword]}*")
 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(url)
-    request["Content-Type"] = 'application/json'
-    request["Accept"] = 'application/json'
-    request["nep-application-key"] = '8a0084a165d712fd0165dbca7b5c0008'
-    request["nep-service-version"] = '2.2.1:2'
-    request["Authorization"] = 'Basic YWNjdDpmaXJlYmFsbC1zdGdAZmlyZWJhbGxzZXJ2aWNldXNlcjpjYXJyb3RsYQ=='
-    request["nep-organization"] = 'ncr-marche-stg'
-    response = http.request(request)
-    puts response.read_body
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(url)
+      request["Content-Type"] = 'application/json'
+      request["Accept"] = 'application/json'
+      request["nep-application-key"] = '8a0084a165d712fd0165dbca7b5c0008'
+      request["nep-service-version"] = '2.2.1:2'
+      request["Authorization"] = 'Basic YWNjdDpmaXJlYmFsbC1zdGdAZmlyZWJhbGxzZXJ2aWNldXNlcjpjYXJyb3RsYQ=='
+      #request["nep-organization"] = 'ncr-marche-stg'
+      response = http.request(request)
+      puts response.read_body
+      parsed = JSON.parse(response.read_body)
+      parsed["pageContent"].each do |page|
+        if page["merchandiseCategory"]["nodeId"]== "Renewable" || page["merchandiseCategory"]["nodeId"] == "Fair-trade" || page["merchandiseCategory"]["nodeId"] == "Recyclable"
+          @results.push(page)
+        end
+      end
+    end
   end
 end
